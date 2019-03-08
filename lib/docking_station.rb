856dbb7ef0ae9cb87a4dfc5827d3aec72c053886
Student <student@Makerss-MacBook-Pro.local>
2019-03-08 13:56:30 +0000

    Up to step 14 from days 2 and 3 on challenge with others. Updating my repo

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,22 @@
-require 'bike'
+require_relative 'bike'
+
 class DockingStation
 
-  def release_bike
-    Bike.new
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
   end
+
+  def release_bike
+    fail 'No bikes available' unless @bikes.count > 0
+    @bikes.pop
+  end
+
+  def dock(our_bike)
+    fail "Docking station full" if @bikes.count == 20
+    @bikes.push(our_bike)
+    @bikes
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,46 @@
 require 'docking_station'
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
-  it 'gets a bike and the bike is working' do
+  describe '#initialize' do
 
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it 'creates an array' do
+      expect(subject.bikes).to eq []
+    end
+
   end
 
+  describe '#release_bike' do
+    it { is_expected.to respond_to :release_bike }
+
+    it 'gets a bike and the bike is working' do
+      subject.dock(Bike.new)
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it 'raise an exception if no bike is available to release' do
+      expect{ subject.release_bike }.to raise_error("No bikes available")
+    end
+  end
+
+  describe '#dock' do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it 'shows the bike that has been docked' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to include bike
+    end
+
+    it 'raise an exception if there is already a bike in the docking station' do
+      20.times { subject.dock Bike.new }
+      expect{ subject.dock(Bike.new) }.to raise_error("Docking station full")
+    end
+
+
+    it 'show a bike has been docked' do
+      my_bike = Bike.new
+      subject.dock(my_bike)
+      expect(subject.bikes).to include my_bike
+    end
+  end
 end
