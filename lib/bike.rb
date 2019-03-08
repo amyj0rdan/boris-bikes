983a44fd8e3a02e07fb424ee6cddeb0d056eff22
Amy Jordan <amy_jordan@live.co.uk>
2019-03-08 15:43:39 +0000

    Created #broken? and #report_broken methods

--- lib/bike.rb
+++ lib/bike.rb
@@ -2,4 +2,13 @@ class Bike
   def working?
     true
   end
+
+  def broken?
+    @broken
+  end
+
+  def report_broken
+    @broken = true
+  end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,14 @@
 require 'bike'
 describe Bike do
   it { is_expected.to respond_to :working? }
+
+  it { is_expected.to respond_to :broken? }
+
+  it { is_expected.to respond_to :report_broken }
+
+  it 'is expected to be broken if reported as broken' do
+    broken_bike = Bike.new
+    broken_bike.report_broken
+    expect(broken_bike.broken?).to eq true
+  end
 end
